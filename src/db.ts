0349dccc9782f1f6ea132a370a7f8dd92acfbd45
Yue Li <yue.li.dev@gmail.com>
2023-07-21 16:09:59 +0200

    continue
    continue

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,9 @@
-import {PrismaClient} from '@prisma/client'
-const globalForPrisma = global as unkown as {prisma: PrismaClient|undefined}
+import { PrismaClient } from '@prisma/client'
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({log: ['query']})
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+}
 
-if(process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
